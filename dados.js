0b83e7a1597e7395f3042869cd9aeb10768744a4
Ingrid Oliveira <contato.ingrid92@gmail.com>
2024-09-13 13:30:36 -0300

    primeiro commit

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, mais conhecida como fadinha, é uma das maiores revelações do skate mundial. A brasileira, conquistou o coração dos fãs com seu talento.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+
+    {
+        titulo : "Rebeca Andrade",
+        descricao :  "É um nome que brilha no cenário esportivo brasileiro. A ginasta, nascida em Guarulhos, São Paulo, conquistou o coração dos brasileiros com sua determinação e talento.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+},
+
+    {
+        titulo : "Beatriz Souza",
+        descricao :  "É um nome que vem se destacando no cenário do judô brasileiro. A atleta, com sua força e técnica impecável, conquistou o país ao garantir a medalha de ouro nos Jogos Olímpicos de Paris 2024.",link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
